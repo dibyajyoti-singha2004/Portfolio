b3daf6bdb83524df19712d37bfb6a6435fa95e7c
Dibyajyoti <mywork7408@gmail.com>
2025-10-19 19:07:40 +0530

    update for vercal

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -22,9 +22,7 @@ const Contact = () => {
     setAlertType(type);
     setAlertMessage(message);
     setShowAlert(true);
-    setTimeout(() => {
-      setShowAlert(false);
-    }, 5000);
+    setTimeout(() => setShowAlert(false), 5000);
   };
 
   const handleSubmit = async (e) => {
@@ -40,7 +38,7 @@ const Contact = () => {
           from_name: formData.name,
           to_name: "Dibyajyoti Singha",
           from_email: formData.email,
-          to_email: "dibyajyotisingha2004@gmail.com",
+          to_email: import.meta.env.VITE_EMAILJS_TO_EMAIL,
           message: formData.message,
         },
         import.meta.env.VITE_EMAILJS_PUBLIC_KEY
@@ -56,31 +54,19 @@ const Contact = () => {
   };
 
   return (
-    <section
-      id="contact"
-      className="relative flex items-center c-space section-spacing"
-    >
-      <Particles
-        className="absolute inset-0 -z-50"
-        quantity={100}
-        ease={80}
-        color={"#ffffff"}
-        refresh
-      />
+    <section id="contact" className="relative flex items-center c-space section-spacing">
+      <Particles className="absolute inset-0 -z-50" quantity={100} ease={80} color={"#ffffff"} refresh />
       {showAlert && <Alert type={alertType} text={alertMessage} />}
       <div className="flex flex-col items-center justify-center max-w-md p-5 mx-auto border border-white/10 rounded-2xl bg-primary">
         <div className="flex flex-col items-start w-full gap-5 mb-10">
           <h2 className="text-heading">Let's Talk</h2>
           <p className="font-normal text-neutral-400">
-            Whether you're looking to build a new website, improve your existing
-            platform, or bring a unique project to life, I'm here to help
+            Whether you're looking to build a new website, improve your existing platform, or bring a unique project to life, I'm here to help
           </p>
         </div>
         <form className="w-full" onSubmit={handleSubmit}>
           <div className="mb-5">
-            <label htmlFor="name" className="feild-label">
-              Full Name
-            </label>
+            <label htmlFor="name" className="feild-label">Full Name</label>
             <input
               id="name"
               name="name"
@@ -94,9 +80,7 @@ const Contact = () => {
             />
           </div>
           <div className="mb-5">
-            <label htmlFor="email" className="feild-label">
-              Email
-            </label>
+            <label htmlFor="email" className="feild-label">Email</label>
             <input
               id="email"
               name="email"
@@ -110,9 +94,7 @@ const Contact = () => {
             />
           </div>
           <div className="mb-5">
-            <label htmlFor="message" className="feild-label">
-              Message
-            </label>
+            <label htmlFor="message" className="feild-label">Message</label>
             <textarea
               id="message"
               name="message"
